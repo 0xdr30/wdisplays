9d7b5b4079d0b644ae616c63a9389a2086b0417d
William Adamson <116238443+0xdr30@users.noreply.github.com>
2025-02-17 15:29:32 -0800

    Update write-to-sway.sh

--- write-to-sway.sh
+++ write-to-sway.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+# William Adamson (0xdr30)
+# 0xdr30@whoppenheimer.com
+# 02/17/2025
+
 # Get list of displays
 displays=$(xrandr | grep " connected" | awk '{print $1}')
 for display in $displays; do 
+  # The resolution of the current display in the series
   resolution=$(xrandr | grep "$display" | awk '/+/{print$3}')
+  # Gets the X axis setting of the display
   x=$(echo $resolution | awk -F'[ +]' '{print $2}')
+  # Same thing with Y axis
   y=$(echo $resolution | awk -F'[ +]' '{print $3}')
+  # Removes the X and Y axis from the resolution string
+  # Example: 1920x1080+0+0 --> 1920x1080
   resolution=$(echo $resolution | awk -F '[ +]' '{print $1}')
-
+  # Sends the display name, coordinates, and resolution to the bottom of the sway config
+  # TODO: Update the line that contains output configuration instead of just adding a new line
   echo "output $display pos $x $y res $resolution" >> ~/.config/sway/config
 done
 
